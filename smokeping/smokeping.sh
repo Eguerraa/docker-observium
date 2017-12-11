4c374d9da4bce65c73ede1066657eaaa2664ad7d
trick77 <jan@trick77.com>
2017-12-11 23:39:36 +0100

    Improved smokeping startup

--- smokeping/smokeping.sh
+++ smokeping/smokeping.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
-#su -l smokeping -s /bin/bash -c "exec /usr/sbin/smokeping --nodaemon"
+#!/bin/bash
+
+[[ -p /tmp/log ]] || mkfifo -m 0660 /tmp/log
 su -l smokeping -s /bin/bash -c "exec /usr/sbin/smokeping --logfile=/tmp/log ${DEBUG:+--debug}"
 tail -F /tmp/log
